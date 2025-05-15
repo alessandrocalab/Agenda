9fd8a3be6b97d10c25b6f026c9bba2b367ef9c5a
alessandrocalab <alessandrocalab3@gmail.com>
2025-05-15 17:07:40 +0200

    Note class implemented

--- src/include/Note.hpp
+++ src/include/Note.hpp
@@ -1,1 +1,61 @@
+#ifndef NOTE_HPP
+#define NOTE_HPP
 
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Note{
+private:
+    std::string date;
+    std::vector<std::string> notes;
+public:
+    Note(const std::string &date):date(date){}
+
+    //setter methods
+    Note &setNotes(const std::vector<std::string> &notes){
+        this->notes=notes;
+        return *this;
+    }
+
+    Note &setDate(const std::string &date){
+        this->date=date;
+        return *this;
+    }
+
+    //add note
+    Note &addNote(const std::string &note){
+        this->notes.push_back(note);
+        return *this;
+    }
+    //remove note
+    bool remNote(unsigned short index){
+        if(index>=notes.size()){
+            std::cerr<<"Notes' index out of range"<<std::endl;
+            return false;
+        }
+
+        notes.erase(notes.begin()+index);
+
+        return true;
+    }
+    //getter methods
+    std::string getDate()const{
+        return date;
+    }
+
+    const std::vector<std::string> &getNotes()const{
+        return notes;
+    }
+    //operator to get notes
+    std::string operator[](unsigned short index)const{
+        if(index>=notes.size()){
+            std::cerr<<"Notes' index out of range"<<std::endl;
+            return "";
+        }
+
+        return notes[index];
+    }
+};
+
+#endif // NOTE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include "include/Date.hpp"
+#include "include/Note.hpp"
 
 using namespace std;
 
 int main()
 {
+    //test Date
     Date oggi;
-
-    char dayname[7][10]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    //char dayname[7][10]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
     oggi.setFormat(1);
-    cout<<oggi.calculateDate(4).getFormattedDate()<<"Day "<<dayname[oggi.getDayName()]<<endl;
+    //cout<<oggi.calculateDate(4).getFormattedDate()<<"Day "<<dayname[oggi.getDayName()]<<endl;
+
+    //test Note
+    Note note(oggi.calculateTodayDate().getFormattedDate());
+    note.addNote("First note");
+    note.addNote("Second note");
+    cout<<"Notes:"<<endl;
+    for(const auto &note : note.getNotes())
+        cout<<note<<endl;
 
 
 
